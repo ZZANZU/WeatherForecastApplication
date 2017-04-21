35bc7fbcd7be1f5779020700a2848096fd2569cb
pjo901018 <pjo901018@korea.ac.kr>
2017-04-22 01:14:33 +0900

    안녕 바보

--- app/src/main/java/com/tistory/dagylo/weatherforecastapplication/MainActivity.java
+++ app/src/main/java/com/tistory/dagylo/weatherforecastapplication/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 
@@ -147,9 +149,30 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        searchView.setOnQueryTextListener(queryTextListener);
+
 
         return true;
     }
+
+    private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+
+            return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+            String TAG = "1";
+            Log.d(TAG, "onQueryTextChange: "+newText);
+
+            // 검색
+            return false;
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
